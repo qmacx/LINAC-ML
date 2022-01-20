8523ea508d07804577aa07f6f1549559f06329d9
lkennedy <lewis.kennedy@cern.ch>
2022-01-20 23:27:31 +0000

    change

--- BeamDNN.py
+++ BeamDNN.py
@@ -2,20 +2,17 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import datetime
 from sklearn.preprocessing import LabelEncoder
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-import seaborn as sns
-from tensorflow import keras
-from keras import layers
-from keras import models 
-from keras import optimizers
-from tensorflow.keras import regularizers, initializers
-from sklearn.metrics import precision_score, recall_score, confusion_matrix, classification_report, accuracy_score, f1_score
+from tensorflow.keras import initializers
+from sklearn.metrics import precision_score, confusion_matrix, classification_report, accuracy_score
 from keras import callbacks
 import tensorflow as tf
+from keras.utils.vis_utils import plot_model
+
+
 
 # Take the evolution of centroid and assign a label to that 
 # labels should be quadrupole element number
@@ -25,20 +22,16 @@ data.columns=['Quad','Angle','CxOTR1','CyOTR1','CxOTR2','CyOTR2','CxOTR3','CyOTR
 df = data.copy()
 
 features = df.drop(['Quad', 'Angle'], axis=1)
-print(features)
-mapping = {'No_PITCH': 0, 'QM1': 1, 'QM2': 2, 'QM3': 3}
-target = df.applymap(lambda x: mapping.get(x) if x in mapping else x)
 target = pd.get_dummies(df['Quad']) 
-print(target)
 
 features_train, features_test, target_train, target_test = train_test_split(features, target, test_size=0.2, random_state = 50)
 
 # model building
-dnn = models.Sequential()
-dnn.add(layers.Dense(units=32, kernel_initializer=initializers.Ones(), activation='relu', input_dim=8))
-dnn.add(layers.Dense(units=16, kernel_initializer='uniform', activation='relu'))
-dnn.add(layers.Dense(units=8, kernel_initializer='uniform', activation='relu'))
-dnn.add(layers.Dense(units=3, kernel_initializer='uniform', activation='softmax')) # softmax clfn
+dnn = tf.keras.models.Sequential()
+dnn.add(tf.keras.layers.Dense(units=32, kernel_initializer=initializers.Ones(), activation='relu', input_dim=8))
+dnn.add(tf.keras.layers.Dense(units=16, kernel_initializer='uniform', activation='relu'))
+dnn.add(tf.keras.layers.Dense(units=8, kernel_initializer='uniform', activation='relu'))
+dnn.add(tf.keras.layers.Dense(units=3, kernel_initializer='uniform', activation='softmax')) # softmax clfn
 
 # compiling
 opt = tf.keras.optimizers.Adam(learning_rate=0.001)
@@ -46,6 +39,9 @@ dnn.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy']
 
 # training
 history = dnn.fit(features_train, target_train, batch_size=32, epochs=150, validation_split=0.2)
+
+# plots
+plot_model(dnn, to_file='dnn_plot.png', show_shapes=True, show_layer_names=True)
 
 history_df = pd.DataFrame(history.history)
 plt.plot(history_df.loc[:, ['loss']], color='blue', label='Training loss')
