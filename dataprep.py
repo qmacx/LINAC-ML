8cc5e1d363ecdbe75a63e347e51f8c2848255619
lkennedy <lewis.kennedy@cern.ch>
2022-03-03 17:27:07 +0000

    8 elements

--- dataprep.py
+++ dataprep.py
@@ -5,11 +5,13 @@ import os, glob
 # Data cleaning
 qmx = glob.glob('./data/QM*x/Cx.txt')
 qmy = glob.glob('./data/QM*y/Cy.txt')
+pmqx = qmx[0:3]
+pmqy = qmy[0:3]
+emqx = qmx[3::]
+emqy = qmy[3::]
+
 bx = glob.glob('./data/B*x/Cx.txt')
 by = glob.glob('./data/B*y/Cy.txt')
-
-qmx_paths = [x for x in qmx]
-qmy_paths = [x for x in qmy]
 bx_paths = [x for x in bx]
 by_paths = [x for x in by]
 
@@ -17,52 +19,56 @@ OTRindex = np.arange(0, 23)
 OTRcols = ['OTR{}'.format(x) for x in OTRindex]
 
 df = pd.DataFrame()
-for i in range(len(qmx_paths)):
-    x = pd.read_csv(qmx_paths[i])
+c = 0
+for i in range(len(pmqx)):
+    x = pd.read_csv(pmqx[i])
     x.columns=[OTRcols]
     x = x.rename(columns={'OTR0': 'dx'}) 
-    x['element'] = np.ones_like(len(x['dx']))*i 
+    x['element'] = np.ones_like(len(x['dx']))*c 
     
-    y = pd.read_csv(qmy_paths[i])
+    y = pd.read_csv(pmqy[i])
     y.columns=[OTRcols]
     y = y.rename(columns={'OTR0': 'dy'}) 
-    y['element'] = np.ones_like(len(y['dy']))*i 
-    
+    y['element'] = np.ones_like(len(y['dy']))*c
+
+    c += 1
     new_df = pd.concat([x, y], axis=0)
     new_df = new_df.fillna(0)
     df = pd.concat([df, new_df], axis=0)
+
 
 for i in range(len(bx_paths)):
     x = pd.read_csv(bx_paths[i])
     x.columns=[OTRcols]
     x = x.rename(columns={'OTR0': 'dx'}) 
-    x['element'] = np.ones_like(len(x['dx']))*i 
+    x['element'] = np.ones_like(len(x['dx']))*c
     
     y = pd.read_csv(by_paths[i])
     y.columns=[OTRcols]
     y = y.rename(columns={'OTR0': 'dy'}) 
-    y['element'] = np.ones_like(len(y['dy']))*i 
+    y['element'] = np.ones_like(len(y['dy']))*c
     
+    c += 1
     new_df = pd.concat([x, y], axis=0)
     new_df = new_df.fillna(0)
     df = pd.concat([df, new_df], axis=0)
 
-print(df.dtypes)
-print(df['OTR22'])
-# Removing unwanted string features in columns
 
-#for i in range(len(df['OTR22'])):
-
-
-
-
-
-
-
-
-
-
-
+for i in range(len(emqx)):
+    x = pd.read_csv(emqx[i])
+    x.columns=[OTRcols]
+    x = x.rename(columns={'OTR0': 'dx'}) 
+    x['element'] = np.ones_like(len(x['dx']))*c
+    
+    y = pd.read_csv(emqy[i])
+    y.columns=[OTRcols]
+    y = y.rename(columns={'OTR0': 'dy'}) 
+    y['element'] = np.ones_like(len(y['dy']))*c
+    
+    c += 1
+    new_df = pd.concat([x, y], axis=0)
+    new_df = new_df.fillna(0)
+    df = pd.concat([df, new_df], axis=0)
 
 
 df.to_csv('./data/data.csv')
