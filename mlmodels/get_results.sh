cd62ed50b9a5747145003c6888e815d570c43499
lkennedy <lewis.kennedy@cern.ch>
2022-04-21 10:55:54 +0100

    compute results

--- mlmodels/get_results.sh
+++ mlmodels/get_results.sh
@@ -2,10 +2,13 @@
 
 > results.txt
 
-python3 pmq_dnn.py >> results.txt &&
-python3 qms_dnn.py >> results.txt &&
-python3 beamline_dnn.py >> results.txt &&
-python3 qms_dnn_otrdrop.py >> results.txt &&
-python3 finalmodel.py >> results.txt &&
+#python3 pmq_dnn.py >> results.txt &&
+#python3 qms_dnn.py >> results.txt &&
+#python3 beamline_dnn.py >> results.txt &&
+#python3 qms_dnn_otrdrop.py >> results.txt &&
+python3 finalclf.py >> results.txt &&
+python3 finalregression.py >> results.txt &&
+python3 clfuncertainty.py >> results.txt &&
+python3 regressionuncertainty.py >> results.txt &&
 
 cat results.txt
